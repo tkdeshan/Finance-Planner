da28c7a49b2428879e9500225cfda87e53e53e62
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-08-26 01:34:00 +0530

    update

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
        mongoose.set('strictQuery', false) 
-       await mongoose.connect(process.env.MONGO_URL)
+       await mongoose.connect(process.env.MONGODB_URI);
        console.log('DB is connected successfully');
     } catch (error) {
         console.log('DB connection Error');
